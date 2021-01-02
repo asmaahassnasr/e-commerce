e24f8b4eae2b7345294684a1572f57d52f62d157
asmaahassnasr <asmaa14has@gmail.com>
2021-01-02 22:07:35 +0200

    Add Validation to create Product

--- controllers/product.js
+++ controllers/product.js
@@ -7,20 +7,39 @@ exports.create = (req , res) => {
     let form = new formidable.IncomingForm(); // all available data here
     form.keepExtensions = true; // All Extensions
     form.parse(req, (err, fields,files) =>{
+        
         if(err) {
             return res.status(400).json({error:"Iamge could not be uploaded"});
         }
+
+        const {name, description,price,category,quantity,shipping} = fields;
+        if(!name || !description || !price || !category || !quantity || !shipping){
+            return res.status(400).json({error:"All Fields are required"});
+        }
+
+
         let product = new Product(fields);
+
         if(files.photo){
+            
+            //if file is greater than 1 mb 
+            if(files.photo.size > 1000000){
+            return res.status(400).json({error:"Iamge Should be less than 1 MB"});
+            }
+
+            
             product.photo.data = fs.readFileSync(files.photo.path);
             product.photo.contentType= files.photo.type;
         }
+
         product.save( (err, result) =>{
             if(err){
                 return res.status(400).json({error:errorHandler(err)})
             }
+
             res.json(result);
         })
+
     });
 }
 
