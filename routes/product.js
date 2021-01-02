98412d917f59e6c699333341b443f588ebb863c1
asmaahassnasr <asmaa14has@gmail.com>
2021-01-03 01:21:40 +0200

    Search product

--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-const {create,productById,read,remove,update,list , listRelated ,listCategories} = require('../controllers/product');
+const {create,productById,read,remove,update,list , listRelated ,listCategories,listBySearch} = require('../controllers/product');
 const {requireSignin,isAdmin,isAuth} = require('../controllers/auth');
 const {userById} = require('../controllers/user');
 
@@ -15,8 +15,12 @@ router.delete("/product/:productId/:userId",requireSignin,isAdmin,isAuth,remove)
 router.put("/product/:productId/:userId",requireSignin,isAdmin,isAuth,update);
 
 router.get('/products', list);
+
 router.get('/products/categories', listCategories);
+
 router.get('/products/related/:productId', listRelated);
+
+router.post("/products/by/search", listBySearch);
 
 router.param('userId',userById);
 router.param('productId',productById);
