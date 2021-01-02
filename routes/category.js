b229de24b7c7de8859222263392a2c1d7e451329
asmaahassnasr <asmaa14has@gmail.com>
2021-01-02 23:37:04 +0200

    Updat , Delelt and get all categories

--- routes/category.js
+++ routes/category.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-const {create, categoryById , read} = require('../controllers/category');
+const {create, categoryById , read ,remove ,update, list} = require('../controllers/category');
 const {requireSignin,isAdmin,isAuth} = require('../controllers/auth');
 const {userById} = require('../controllers/user');
 
-
 router.get("/category/:categoryId",read);
 
-
 router.post("/category/create/:userId",requireSignin,isAdmin,isAuth,create);
+
+router.delete("/category/:categoryId/:userId",requireSignin,isAdmin,isAuth,remove);
+
+router.put("/category/:categoryId/:userId",requireSignin,isAdmin,isAuth,update);
+
+router.get("/categories", list);
+
 
 router.param('categoryId',categoryById);
 router.param('userId',userById);
