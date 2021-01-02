2e93b2f69805c6b1996c969ceb6101aea064f93b
asmaahassnasr <asmaa14has@gmail.com>
2021-01-02 23:12:56 +0200

    get single category by id

--- controllers/category.js
+++ controllers/category.js
@@ -1,6 +1,21 @@
 const Category = require('../models/category');
 const {errorHandler} =require('../helpers/dbErrorHandler');
 
+exports.categoryById = (req , res ,next,id ) =>{
+ Category.findById(id).exec( (err, category) =>{
+    if(err || !category){
+        return res.status(400).
+        json({error:"category not found"});
+    }
+    req.category = category;
+    next();
+ })
+}
+
+
+exports.read =(req , res) =>{
+    return res.json(req.category);
+}
 
 exports.create = (req , res) => {
     const category = new Category (req.body);
