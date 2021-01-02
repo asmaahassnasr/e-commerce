0f0a9d9ca035f628247d08fe987ddcca7dab7103
asmaahassnasr <asmaa14has@gmail.com>
2021-01-03 00:40:48 +0200

    Products by sell and arrival on request query params

--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-const {create,productById,read,remove,update} = require('../controllers/product');
+const {create,productById,read,remove,update,list} = require('../controllers/product');
 const {requireSignin,isAdmin,isAuth} = require('../controllers/auth');
 const {userById} = require('../controllers/user');
 
@@ -13,6 +13,8 @@ router.post("/product/create/:userId",requireSignin,isAdmin,isAuth,create);
 router.delete("/product/:productId/:userId",requireSignin,isAdmin,isAuth,remove);
 
 router.put("/product/:productId/:userId",requireSignin,isAdmin,isAuth,update);
+
+router.get('/products', list);
 
 router.param('userId',userById);
 router.param('productId',productById);
