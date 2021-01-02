a87f005c0279970966f7ae9640174cf43c40d091
asmaahassnasr <asmaa14has@gmail.com>
2021-01-02 16:01:45 +0200

    Refactoring names of files

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 //Imports 
-const userRoutes = require('./routes/user');
+const authRoutes = require('./routes/auth');
 
 //App 
 const app = express();
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(expressValidator());
 
 //Routes Middlewar
-app.use('/api',userRoutes);
+app.use('/api',authRoutes);
 
 const port = process.env.PORT || 8000
 
